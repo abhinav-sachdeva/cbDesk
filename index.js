75547de4603ff5f0e285cdabfa69ad3cf07ccc31
Abhinav Sachdeva <happy257@gmail.com>
2018-09-08 14:34:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+// SET ENV
+process.env.NODE_ENV = 'development';
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+
+  // Listen for app to be ready
+app.on('ready', () => {
+    // Create new window
+    mainWindow = new BrowserWindow({
+        transparent: true,
+        frame: false,
+        alwaysOnTop:true,
+        skipTaskbar :true,
+        width: 400, height: 500,
+        resizable :false,
+        x:electron.screen.getPrimaryDisplay().size.width-400,
+        y:150,
+        backgroundColor :"#fff"
+    });
+    // Load html in window
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'mainWindow.html'),
+      protocol: 'file:',
+      slashes:true
+    }));
+    // Quit app when closed
+    mainWindow.on('closed', () => {
+      app.quit();
+    });
+  
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+  }
+
+)
+
+// Create menu template
+const mainMenuTemplate =  [
+  // Each object is a dropdown
+  {
+    label: 'File',
+    submenu:[
+      {
+        label:'Add Item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label:'Clear Items',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// If OSX, add empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
